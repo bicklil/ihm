3cc4e636bc8d42efa12b86b59331f8761f207526
bicklil <lemanjc@gmail.com>
2017-01-18 17:24:25 +0100

    mise au norme de pep8

--- tp1.py
+++ tp1.py
@@ -2,63 +2,66 @@ import tkinter as tk
 
 
 def ouvrir_fichier(file):
-# ouvre un fichier 
-#si reussi retourne le file descriptor
-#si echoue affiche les erreurs d ouverture 
-	try:
-		file_open = open(file,"r")
-		print("reussi\n")
-		return file_open
-	except OSError as e:
-		print("errno: ",e.errno)
-		print("filename: ",e.filename)
-		print("strerror: ",e.strerror)
-#fermeture du bin crash		()
-		
+    """ ouvre un fichier
+    si reussi retourne le file descriptor
+    si echoue affiche les erreurs d ouverture"""
+    try:
+        file_open = open(file, "r")
+        print("reussi\n")
+        return file_open
+    except OSError as e:
+        print("errno: ", e.errno)
+        print("filename: ", e.filename)
+        print("strerror: ", e.strerror)
+        exit()
+
+
 def parser_rgb(file_open):
-# recupere a partir du file descriptor toutes les couleurs
-#enleve les doublons 
-#et renvoie un dictionnaire 
-    
-    next(file_open) # on saute la premiere ligne du rgb elle sert a rien
+    """ recupere a partir du file descriptor toutes les couleurs
+    enleve les doublons
+    et renvoie un dictionnaire """
+    next(file_open)  # on saute la premiere ligne du rgb elle sert a rien
     dico = {}
-    for line in file_open :
+    for line in file_open:
         linecollapse = line.split()
-        
-        if len(linecollapse) == 5: #on test s'il y a un espace dans le nom de la couleur si oui on l'enleve
+# on test s'il y a un espace dans le nom de la couleur si oui on l'enleve
+        if len(linecollapse) == 5:
             linecollapse[3] = linecollapse[3]+linecollapse[4]
-            
+
         linecollapse[3] = linecollapse[3].lower()
         test = False
-        for color in dico: # on regarde si la couleur est pas deja mis
-            if color == linecollapse[3] :
-                test = True     
+        for color in dico:  # on regarde si la couleur est pas deja mis
+            if color == linecollapse[3]:
+                test = True
                 break
-                
-        if test == False:
-            dico[linecollapse[3]] = (int(linecollapse[0]),int(linecollapse[1]),int(linecollapse[2]))
-    
+
+        if not test:
+            dico[linecollapse[3]] = (int(linecollapse[0]),
+                                     int(linecollapse[1]),
+                                     int(linecollapse[2]))
+
     return dico
-	
+
+
 def color_alpha(dico):
-#prends le dictionaire de couleur
-#mets chaque element dans un tableau
-#et on le trie 
+    """prends le dictionaire de couleur
+    mets chaque element dans un tableau
+    et on le trie"""
     tab_color = []
     for color in dico:
         color = color[0].upper() + color[1:]
-        i=1
-        while color[-i].isdigit(): # on regarde si la couleur finit par un nombre
-            i+=1
-            
-        if i!=1: #si on a une fin avec un chiffre on ajoute un espace
-            i-=1
+        i = 1
+        # on regarde si la couleur finit par un nombre
+        while color[-i].isdigit():
+            i += 1
+
+        if i != 1:  # si on a une fin avec un chiffre on ajoute un espace
+            i -= 1
             color = color[:-i]+" "+color[-i:]
-            
+
         tab_color.append(color)
     tab_color.sort()
     return tab_color
-
 
 
 if __name__ == "__main__":
@@ -70,17 +73,16 @@ if __name__ == "__main__":
     ftop1 = tk.Toplevel(root)
 
     var = tk.StringVar()
-    
+
     canv = tk.Canvas(ftop1)
     canv.pack()
-    
+
     fram = tk.Frame(ftop1)
     fram.pack()
-    
+
     boutton = {}
-    for mesg in ("Ok","Annuler"):
-        boutton[mesg]=tk.Button(fram,text=mesg)
+    for mesg in ("Ok", "Annuler"):
+        boutton[mesg] = tk.Button(fram, text=mesg)
         boutton[mesg].pack(side="left")
-    
-    
+
     root.mainloop()
