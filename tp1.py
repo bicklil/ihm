2ea9cc44e80d171b953b6d7f50b27dbe6c6b415b
bicklil <lemanjc@gmail.com>
2017-01-18 07:51:06 +0100

    ajout des coms

--- tp1.py
+++ tp1.py
@@ -2,6 +2,9 @@ import tkinter as tk
 
 
 def ouvrir_fichier(file):
+# ouvre un fichier 
+#si reussi retourne le file descriptor
+#si echoue affiche les erreurs d ouverture 
 	try:
 		file_open = open(file,"r")
 		print("reussi\n")
@@ -10,22 +13,26 @@ def ouvrir_fichier(file):
 		print("errno: ",e.errno)
 		print("filename: ",e.filename)
 		print("strerror: ",e.strerror)
+#fermeture du bin crash		()
 		
 def parser_rgb(file_open):
+# recupere a partir du file descriptor toutes les couleurs
+#enleve les doublons 
+#et renvoie un dictionnaire 
     
-    next(file_open)
+    next(file_open) # on saute la premiere ligne du rgb elle sert a rien
     dico = {}
     for line in file_open :
         linecollapse = line.split()
         
-        if len(linecollapse) == 5:
+        if len(linecollapse) == 5: #on test s'il y a un espace dans le nom de la couleur si oui on l'enleve
             linecollapse[3] = linecollapse[3]+linecollapse[4]
             
         linecollapse[3] = linecollapse[3].lower()
         test = False
-        for color in dico:
+        for color in dico: # on regarde si la couleur est pas deja mis
             if color == linecollapse[3] :
-                test = True
+                test = True     
                 break
                 
         if test == False:
@@ -34,14 +41,17 @@ def parser_rgb(file_open):
     return dico
 	
 def color_alpha(dico):
+#prends le dictionaire de couleur
+#mets chaque element dans un tableau
+#et on le trie 
     tab_color = []
     for color in dico:
         color = color[0].upper() + color[1:]
         i=1
-        while color[-i].isdigit():
+        while color[-i].isdigit(): # on regarde si la couleur finit par un nombre
             i+=1
             
-        if i!=1: #on a une fin avec un chiffre
+        if i!=1: #si on a une fin avec un chiffre on ajoute un espace
             i-=1
             color = color[:-i]+" "+color[-i:]
             
@@ -60,14 +70,17 @@ if __name__ == "__main__":
     ftop1 = tk.Toplevel(root)
 
     var = tk.StringVar()
+    
     canv = tk.Canvas(ftop1)
     canv.pack()
+    
     fram = tk.Frame(ftop1)
     fram.pack()
+    
     boutton = {}
-    for mesg in ("ok","oops"):
+    for mesg in ("Ok","Annuler"):
         boutton[mesg]=tk.Button(fram,text=mesg)
         boutton[mesg].pack(side="left")
-        
+    
+    
     root.mainloop()
-		
