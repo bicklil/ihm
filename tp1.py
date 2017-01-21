3d3427ae867542877b98fb8787b4610119b3fc31
bicklil <lemanjc@gmail.com>
2017-01-21 11:16:02 +0100

    ajout de com

--- tp1.py
+++ tp1.py
@@ -14,39 +14,39 @@ def mouse_wheel(event):
 def couleur_surligne(event):
     """ callback modifier le contour
     d'un rectangle au survol de la souris """
-    canv = event.widget
-    rect = canv.find_withtag("current")[0]
-    canv.itemconfig(rect, outline="red")
-    canv.itemconfig(rect, width=1.5)
+    Canv = event.widget
+    Rect = Canv.find_withtag("current")[0]
+    Canv.itemconfig(Rect, outline="red")
+    Canv.itemconfig(Rect, width=1.5)
 
 
 def couleur_desurligne(event):
     """ callback remetant a l'etat initial si
     la souris sort d'un rectangle"""
-    canv = event.widget
-    rect = canv.find_withtag("current")[0]
-    canv.itemconfig(rect, outline="black")
-    canv.itemconfig(rect, width=1)
+    Canv = event.widget
+    Rect = Canv.find_withtag("current")[0]
+    Canv.itemconfig(Rect, outline="black")
+    Canv.itemconfig(Rect, width=1)
 
 
-def click_ok(lab, var, ftop):
+def click_ok(Lab, Var, TopLevel):
     """ callback sur le click du bouton ok
     permettant de sauvegarder la derniere couleur cliqué
     et de close la fenetre"""
-    var.set(lab["text"])
-    print(var.get())
-    ftop.destroy()
+    Var.set(Lab["text"])
+    print(Var.get())
+    TopLevel.destroy()
 
 
-def click_annuler(ftop):
+def click_annuler(TopLevel):
     """ callback du clique annuler qui ferme la fenetre"""
-    ftop.destroy()
+    TopLevel.destroy()
 
 
-def clic_couleur(color,  lab):
+def clic_couleur(Color,  Lab):
     """ callback apres avoir click sur une couleur
     qui implique une mise a jour du texte"""
-    lab["text"] = color
+    Lab["text"] = Color
 
 
 def ouvrir_fichier(file):
@@ -95,103 +95,103 @@ def color_alpha(dico):
     """prends le dictionaire de couleur
     mets chaque element dans un tableau
     et on le trie"""
-    tab_color = []
-    for color in dico:
-        color = color[0].upper() + color[1:]
+    TabColor = []
+    for Color in dico:
+        Color = Color[0].upper() + Color[1:]
         i = 1
         # on regarde si la couleur finit par un nombre
-        while color[-i].isdigit():
+        while Color[-i].isdigit():
             i += 1
 
         if i != 1:  # si on a une fin avec un chiffre on ajoute un espace
             i -= 1
-            color = color[:-i]+" "+color[-i:]
+            Color = Color[:-i]+" "+Color[-i:]
 
-        tab_color.append(color)
-    tab_color.sort()
-    return tab_color
+        TabColor.append(Color)
+    TabColor.sort()
+    return TabColor
 
 
 def bazarre():
     """ fonction a split en plusieur sous fonction"""
-    rgbtxt = ouvrir_fichier("/etc/X11/rgb.txt")
-    dico_color = parser_rgb(rgbtxt)
-    rgbtxt.close()
+    RgbTxt = ouvrir_fichier("/etc/X11/rgb.txt")
+    DicoColor = parser_rgb(RgbTxt)
+    RgbTxt.close()
 
     # init de variable
-    tab_color = color_alpha(dico_color)
-    nb_color = len(tab_color)
-    nb_colonne = 18
-    nb_ligne = (nb_color)/nb_colonne
-    nb_ligne_a_afficher = 10
-    cote_square = 20
-    ecart = 2
-    if nb_color % nb_colonne != 0:
-        nb_ligne += 1
+    TabColor = color_alpha(DicoColor)
+    NbColor = len(TabColor)
+    NbColonne = 18
+    NbLigne = (NbColor)/NbColonne
+    NbLigne_a_afficher = 10
+    CoteCarre = 20
+    Ecart = 2
+    if NbColor % NbColonne != 0:
+        NbLigne += 1
 
     # initialisation des principaux éléments
-    root = tk.Tk()
-    ftop1 = tk.Toplevel(root)
-    var = tk.StringVar()
-    var.set("NULL")
-    canv = tk.Canvas(ftop1, width=ecart+nb_colonne * (cote_square+ecart),
-                     height=ecart+nb_ligne_a_afficher * (cote_square+ecart),
+    Root = tk.Tk()
+    TopLevel1 = tk.Toplevel(Root)
+    Var = tk.StringVar()
+    Var.set("NULL")
+    Canv = tk.Canvas(TopLevel1, width=Ecart+NbColonne * (CoteCarre+Ecart),
+                     height=Ecart+NbLigne_a_afficher * (CoteCarre+Ecart),
                      scrollregion=(0, 0, 0,
-                     ecart+nb_ligne * (cote_square+ecart)))
-    fram = tk.Frame(ftop1)
-    lab = tk.Label(ftop1, text=var.get())
-    scroll = tk.Scrollbar(ftop1)
+                     Ecart+NbLigne * (CoteCarre+Ecart)))
+    Fram = tk.Frame(TopLevel1)
+    Lab = tk.Label(TopLevel1, text=Var.get())
+    scroll = tk.Scrollbar(TopLevel1)
 
     # placement de la premiere couche d'element
-    lab.pack(side="top")
+    Lab.pack(side="top")
     scroll.pack(side="right", fill=tk.Y)
     # mise en place de la scrollbar
-    scroll.config(command=canv.yview)
-    canv.config(yscrollcommand=scroll.set)
-    canv.pack()
-    fram.pack(side="bottom")
+    scroll.config(command=Canv.yview)
+    Canv.config(yscrollcommand=scroll.set)
+    Canv.pack()
+    Fram.pack(side="bottom")
 
-    boutton = {}  # mise en place des boutons
-    for mesg in ("Ok", "Annuler"):
-        boutton[mesg] = tk.Button(fram, text=mesg)
-        boutton[mesg].pack(side="left")
+    Boutton = {}  # mise en place des boutons
+    for Mesg in ("Ok", "Annuler"):
+        Boutton[Mesg] = tk.Button(Fram, text=Mesg)
+        Boutton[Mesg].pack(side="left")
 
-    boutton["Ok"].config(command=lambda: click_ok(lab, var, ftop1))
-    boutton["Annuler"].config(command=lambda: click_annuler(ftop1))
+    Boutton["Ok"].config(command=lambda: click_ok(Lab, Var, TopLevel1))
+    Boutton["Annuler"].config(command=lambda: click_annuler(TopLevel1))
     # mise en place des couleurs dans des rectangles
     # chaque rectangle a comme tag le nom de la couleur et le tag "couleur"
-    square_color = {}
-    canv_ligne = {}
+    SquareColor = {}
     indice_ligne = -1
     i = 0
-    for color in sorted(dico_color.keys()):
+    for Color in sorted(DicoColor.keys()):
         if i == 0:
             indice_ligne += 1
-        bg_color = '#{0:02x}{1:02x}{2:02x}'.format(dico_color[color][0],
-                                                   dico_color[color][1],
-                                                   dico_color[color][2])
-        x1 = i*cote_square + ecart + i*ecart
-        y1 = ecart+cote_square*indice_ligne + ecart * indice_ligne
-        x2 = (i+1)*cote_square + (i+1)*ecart
-        y2 = cote_square*(indice_ligne+1) + ecart * (indice_ligne+1)
+        bg_color = '#{0:02x}{1:02x}{2:02x}'.format(DicoColor[Color][0],
+                                                   DicoColor[Color][1],
+                                                   DicoColor[Color][2])
+        # calcul des coordonnées des rectangles
+        x1 = i*CoteCarre + Ecart + i*Ecart
+        y1 = Ecart+CoteCarre*indice_ligne + Ecart * indice_ligne
+        x2 = (i+1)*CoteCarre + (i+1)*Ecart
+        y2 = CoteCarre*(indice_ligne+1) + Ecart * (indice_ligne+1)
 
-        square_color[color] = canv.create_rectangle(
+        SquareColor[Color] = Canv.create_rectangle(
                                     x1, y1, x2, y2,
-                                    fill=bg_color, tags=("couleur", color))
+                                    fill=bg_color, tags=("couleur", Color))
         # ajout du tag bind pour le click sur une couleur
-        canv.tag_bind(color, "<Button-1>",
-                      lambda event, color=color:
-                      clic_couleur(color, lab))
-        canv.tag_bind(color, "<Enter>", couleur_surligne)
-        canv.tag_bind(color, "<Leave>", couleur_desurligne)
+        Canv.tag_bind(Color, "<Button-1>",
+                      lambda event, Color=Color:
+                      clic_couleur(Color, Lab))
+        Canv.tag_bind(Color, "<Enter>", couleur_surligne)
+        Canv.tag_bind(Color, "<Leave>", couleur_desurligne)
         i += 1
-        i = i % nb_colonne
+        i = i % NbColonne
 
-    canv.bind("<Button-4>", mouse_wheel)
-    canv.bind("<Button-5>", mouse_wheel)
-    ftop1.grab_set()
-    ftop1.focus_set()
-    root.mainloop()
+    Canv.bind("<Button-4>", mouse_wheel)
+    Canv.bind("<Button-5>", mouse_wheel)
+    TopLevel1.grab_set()
+    TopLevel1.focus_set()
+    Root.mainloop()
 
 
 if __name__ == "__main__":
