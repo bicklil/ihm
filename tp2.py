f5916a720c28041b1239b71a0b7254ce4ed449a1
bicklil <lemanjc@gmail.com>
2017-01-22 20:47:30 +0100

    ajout barre de menu

--- tp2.py
+++ tp2.py
@@ -3,3 +3,13 @@ import tkinter as tk
 
 if __name__ == "__main__":
     Root = tk.Tk()
+
+    MenuBar = tk.Menu(Root)
+    Root.config(menu=MenuBar)
+
+    MenuFichier = tk.Menu(MenuBar, tearoff=0)
+    MenuBar.add_cascade(label="Fichier", menu=MenuFichier)
+    MenuAide = tk.Menu(MenuBar, tearoff=0)
+    MenuBar.add_cascade(label="Aide", menu=MenuAide)
+
+    Root.mainloop()
