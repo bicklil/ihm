44ed103d1147ed856e8bbf58f0dfbfaaacf55c86
bicklil <lemanjc@gmail.com>
2017-01-23 07:26:44 +0100

    mise en place des fonctions de base

--- tp2.py
+++ tp2.py
@@ -1,15 +1,28 @@
 import tkinter as tk
 
 
-if __name__ == "__main__":
+def initialisation_tk():
     Root = tk.Tk()
-
+    Canv = tk.Canvas(Root)
+    Label = tk.Label(Root, text="test")
     MenuBar = tk.Menu(Root)
-    Root.config(menu=MenuBar)
-
     MenuFichier = tk.Menu(MenuBar, tearoff=0)
-    MenuBar.add_cascade(label="Fichier", menu=MenuFichier)
-    MenuAide = tk.Menu(MenuBar, tearoff=0)
-    MenuBar.add_cascade(label="Aide", menu=MenuAide)
+    return(Root, Canv, Label, MenuBar, MenuFichier)
 
+
+def configuration_tk(Root, Menubar, MenuFichier):
+    Root.config(menu=MenuBar)
+    MenuBar.add_cascade(label="Fichier", menu=MenuFichier)
+
+
+def placement_tk(Label, Canv):
+    Label.pack(side="bottom", fill=tk.X)
+    Canv.pack()
+
+
+if __name__ == "__main__":
+
+    (Root, Canv, Label, MenuBar, MenuFichier) = initialisation_tk()
+    configuration_tk(Root, MenuBar, MenuFichier)
+    placement_tk(Label, Canv)
     Root.mainloop()
