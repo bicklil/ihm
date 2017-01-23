6379ff0d164357718b2063b9be74e586829bfc00
bicklil <lemanjc@gmail.com>
2017-01-23 07:34:30 +0100

    mise en place des fonctions de base

--- tp2.py
+++ tp2.py
@@ -1,7 +1,12 @@
 import tkinter as tk
 
 
+def ctrl_click(event):
+    pass
+
+
 def initialisation_tk():
+    """prepare tous les widgets necessaires à l'appli"""
     Root = tk.Tk()
     Canv = tk.Canvas(Root)
     Label = tk.Label(Root, text="test")
@@ -11,13 +16,19 @@ def initialisation_tk():
 
 
 def configuration_tk(Root, Menubar, MenuFichier):
+    """modifie les widgets"""
     Root.config(menu=MenuBar)
     MenuBar.add_cascade(label="Fichier", menu=MenuFichier)
 
 
 def placement_tk(Label, Canv):
+    """place les widgets sur le canv"""
     Label.pack(side="bottom", fill=tk.X)
     Canv.pack()
+
+
+def Canv_call(Canv):
+    pass
 
 
 if __name__ == "__main__":
@@ -25,4 +36,5 @@ if __name__ == "__main__":
     (Root, Canv, Label, MenuBar, MenuFichier) = initialisation_tk()
     configuration_tk(Root, MenuBar, MenuFichier)
     placement_tk(Label, Canv)
+    Canv_call(Canv)
     Root.mainloop()
